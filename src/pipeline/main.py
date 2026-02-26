21cad135f0460e19a59345ce4e1801fff320d494
SchmidtCode <rayhschmidt@gmail.com>
2026-02-26 00:10:23 -0500

    removed old compose version and updated main.py to 46:00

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
-from prefect import flow
+import httpx
+from prefect import flow, get_run_logger
 
 
 @flow
 def collect_petstore_inventory():
-    print("Collecting inventory from the Petstore API...")
+    logger = get_run_logger()
+    url = "https://petstore.swagger.io/v2/store/inventory"
+    response = httpx.get(url)
+    response.raise_for_status()
+    inventory_stats = response.json()
+    logger.info(inventory_stats)
 
 
 def main():
-    print("hello world")
+    collect_petstore_inventory.serve("petstore-collection-deployment")
 
 
 if __name__ == "__main__":
