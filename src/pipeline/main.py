e652ecebc983fd03792a6a303131e771b9738f3c
SchmidtCode <rayhschmidt@gmail.com>
2026-02-26 00:26:08 -0500

    broke into task and flow 53:50

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
 
 import httpx
-from prefect import flow, get_run_logger
+from prefect import flow, get_run_logger, task
 
 
-@flow
-def collect_petstore_inventory():
+@task
+def retrieve_from_api(
+    base_url: str,
+    path: str,
+    secure: bool,
+):
     logger = get_run_logger()
-    url = "https://petstore.swagger.io/v2/store/inventory"
+    if secure:
+        url = f"https://{base_url}{path}"
+    else:
+        url = f"http://{base_url}{path}"
     response = httpx.get(url)
     response.raise_for_status()
     inventory_stats = response.json()
     logger.info(inventory_stats)
+
+
+@flow
+def collect_petstore_inventory(
+    base_url="petstore.swagger.io",
+    path="/v2/store/inventory",
+    secure=True,
+):
+    inventory_stats = retrieve_from_api(
+        base_url,
+        path,
+        secure,
+    )
 
 
 def main():
