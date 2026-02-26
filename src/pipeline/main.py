f97dda81aa3f67198236ac1f1fad3c555c407422
SchmidtCode <rayhschmidt@gmail.com>
2026-02-25 23:40:43 -0500

    Configure packaging and pipeline entrypoint

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+from prefect import flow
+
+
+@flow
+def collect_petstore_inventory():
+    print("Collecting inventory from the Petstore API...")
+
 
 def main():
     print("hello world")
